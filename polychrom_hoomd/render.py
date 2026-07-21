abd6f9965b482e17043b86f1325296d502a2f34c
ljw <ljw2017@sjtu.edu.cn>
2026-07-21 11:06:25 +0800

    Close auto formatter.

--- polychrom_hoomd/render.py
+++ polychrom_hoomd/render.py
@@ -10,52 +10,50 @@ except ImportError:
 from matplotlib.colors import Normalize
 
 
-def domain_viewer(snap, cmap="coolwarm", **kwargs):
+def domain_viewer(snap, cmap='coolwarm', **kwargs):
     """
     Visualize chromatin domains per chromosome in 1D
     """
-
+        
     chrom_bounds = utils.get_chrom_bounds(snap)
     chrom_lengths = np.diff(chrom_bounds, axis=1).flatten() + 1
-
+        
     typeids = snap.particles.typeid.copy()
     vmin, vmax = typeids.min(), typeids.max()
-
+    
     number_of_monomers = sum(chrom_lengths)
     try:
         colors = get_cmap(cmap)(Normalize(vmin=vmin, vmax=vmax)(typeids))
     except NameError:
         colors = colormaps[cmap](Normalize(vmin=vmin, vmax=vmax)(typeids))
-
-    colors[:, 3] = 1.0
+    
+    colors[:, 3] = 1.
     colors = colors[:number_of_monomers]
-
+    
     viewers.chromosome_viewer(chrom_lengths, colors, **kwargs)
 
 
-def fresnel(
-    snap,
-    cmap="viridis",
-    rescale_backbone_bonds=1.0,
-    show=None,
-    color_array=None,
-    **kwargs,
-):
+def fresnel(snap,
+            cmap='viridis',
+            rescale_backbone_bonds=1.,
+            show=None,
+            color_array=None,
+            **kwargs):
     """
     Wrapper around polykit.renderers.backends for HooMD rendering using the Fresnel library
     """
 
     bonds = snap.bonds.group.copy()
     positions = utils.unwrap_coordinates(snap)
-
+        
     bond_mask = np.ones(snap.particles.N, dtype=bool)
     polymer_mask = np.ones(snap.particles.N, dtype=bool)
 
-    LEF_typeid = snap.bonds.types.index("LEF") if "LEF" in snap.bonds.types else -1
+    LEF_typeid = snap.bonds.types.index('LEF') if 'LEF' in snap.bonds.types else -1
 
     polymer_mask[bonds] = False
     bond_mask[bonds[snap.bonds.typeid == LEF_typeid]] = False
-
+    
     bond_mask[polymer_mask] = False
 
     radii = snap.particles.diameter.copy() * 0.5
@@ -63,32 +61,32 @@ def fresnel(
 
     if isinstance(color_array, np.ndarray):
         colorscale = color_array
-
+        
     else:
         colorscale = np.zeros(snap.particles.N)
-
+    
         if show == "chromosomes":
             chrom_bounds = utils.get_chrom_bounds(snap)
-
+                    
             for i, bounds in enumerate(chrom_bounds):
-                colorscale[bounds[0] : bounds[1] + 1] = i + 1
-
+                colorscale[bounds[0]:bounds[1]+1] = i+1
+                            
         elif show == "loops":
             loop_bounds = bonds[snap.bonds.typeid == LEF_typeid]
-
+                    
             for i, bounds in enumerate(loop_bounds):
-                colorscale[bounds[0] : bounds[1] + 1] = i + 1
-
+                colorscale[bounds[0]:bounds[1]+1] = i+1
+                        
         elif show == "compartments":
             colorscale = snap.particles.typeid.copy()
-
+            
         elif show == "strains":
             strains = np.diff(positions[bonds], axis=1)
             colorscale = np.linalg.norm(strains, axis=-1).flatten()
-
+            
         else:
             colorscale = np.arange(snap.particles.N)
-
+    
     try:
         colors = get_cmap(cmap)(Normalize()(colorscale))
     except NameError:
